a47e93db0d2f46b6cbb6b9588c80eaf2027d98ab
yiming.wang <983990712@qq.com>
2026-06-19 02:01:52 +0800

    feat(news): create /news/ page with NewsList

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,12 @@
 import DefaultTheme from 'vitepress/theme'
 import LatestNews from './components/LatestNews.vue'
+import NewsList from './components/NewsList.vue'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('LatestNews', LatestNews)
+    app.component('NewsList', NewsList)
   }
 }
